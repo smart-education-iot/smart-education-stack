b10921c3911ce3387de63be6a8057c5ec01cccd0
james li <li.jin.feng.2008@163.com>
2017-03-20 21:59:27 +0800

    add five modules

---
+++ education-website/src/main/java/com/education/website/App.java
@@ -0,0 +1,13 @@
+package com.education.website;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
